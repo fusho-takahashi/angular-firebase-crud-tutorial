6887a3f8d8de2ad20734feef192a83147f68487f
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-01-10 19:05:51 +0900

    policy model 定義

--- src/app/policy.model.ts
+++ src/app/policy.model.ts
@@ -1,2 +1,10 @@
 export class Policy {
+  id: string;
+  policyNumber: string;
+  creationDate: Date;
+  effectiveDate: Date;
+  expireDate: Date;
+  paymentOption: string;
+  policyAmount: number;
+  extraInfo: string;
 }
