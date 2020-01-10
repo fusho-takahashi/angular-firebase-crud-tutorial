a73b002be45536502addaf9f425836ce107ddb4d
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-01-10 19:24:56 +0900

    component の実装（動かない）

--- src/app/policy-list/policy-list.component.ts
+++ src/app/policy-list/policy-list.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { PolicyService } from '../policy.service';
+import { Policy } from '../policy.model';
 
 @Component({
   selector: 'app-policy-list',
   templateUrl: './policy-list.component.html',
-  styleUrls: ['./policy-list.component.scss']
+  styleUrls: ['./policy-list.component.scss'],
 })
 export class PolicyListComponent implements OnInit {
+  constructor(private policyService: PolicyService) {}
 
-  constructor() { }
+  policies: Policy[];
 
   ngOnInit() {
+    this.policyService.getPolicies().subscribe((data) => {
+      this.policies = data.map((e) => {
+        return {
+          id: e.payload.doc.id,
+          ...e.payload.doc.data,
+        } as Policy;
+      });
+    });
   }
 
+  create(policy: Policy) {
+    this.policyService.createPolicy(policy);
+  }
+
+  update(policy: Policy) {
+    this.policyService.updatePolicy(policy);
+  }
+
+  delete(id: string) {
+    this.policyService.deletePolicy(id);
+  }
 }
