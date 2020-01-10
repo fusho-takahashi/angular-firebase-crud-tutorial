54f23625e63628eba00e5f99dc63d735b53f5ed6
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-01-10 18:52:06 +0900

    firebase とfirestore の設定

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireDatabaseModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
   production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBAgqwKBE5lzpzPYkugbZ1GT1fYfyUkPyM',
+    authDomain: 'angular-firebase-crud-71cce.firebaseapp.com',
+    databaseURL: 'https://angular-firebase-crud-71cce.firebaseio.com',
+    projectId: 'angular-firebase-crud-71cce',
+    storageBucket: 'angular-firebase-crud-71cce.appspot.com',
+    messagingSenderId: '415768915402',
+    appId: '1:415768915402:web:081fe05a10ef1cf414f333',
+    measurementId: 'G-98BMBL38FQ',
+  },
 };
 
 /*
