86432c451884e4ad1dfc718c12a433ee9547fb00
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-01-10 19:14:46 +0900

    ng g c policy-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from '../environments/environment';
+import { PolicyListComponent } from './policy-list/policy-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PolicyListComponent],
   imports: [BrowserModule, AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireDatabaseModule],
   providers: [],
   bootstrap: [AppComponent],
